c9749d8f6996fc042447b9063c27517f428be625
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-05-13 08:33:50 -0300

    Fixed database connection error

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php require('./src/db_conn.php'); ?>
+<?php
+    require('./src/db_conn.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,38 +12,38 @@
     <link rel="stylesheet" href="./public/css/index.css">
     <title>Lista dos Afazeres</title>
 </head>
-<body>
-    <div class="main-section">
-    <!-- adicionando sessão -->
-        <div class="add-section">
-            <form action="">
-                <input type="text" name="title" id="title" placeholder="Este campo é requerido..">
-                <button type="submit">Add &nbsp; <span>&#43;</span></button>
-            </form>
-        </div>
-        
-        <?php 
-            $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
-        ?>
-        <!-- Exibindo a sessão-->
-        <div class="show-todo-section">
+    <body>
+        <div class="main-section">
+        <!-- adicionando sessão -->
+            <div class="add-section">
+                <form action="">
+                    <input type="text" name="title" id="title" placeholder="Este campo é requerido..">
+                    <button type="submit">Add &nbsp; <span>&#43;</span></button>
+                </form>
+            </div>
 
-            <?php if($todos->rowCount() === 0){ ?>
+            <!-- Exibindo a sessão-->
+            <?php 
+                $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+              ?>
+
+            <div class="show-todo-section">
+                <?php if ($todos->rowCount() > 0) { ?>
+                        <div class="todo-item">
+                            <input type="checkbox">
+                            <div class="empty">>
+                                <img src="./public/img/f.png" width="100%" alt="f">
+                                <img src="./public/img/Ellipsis.gif" width="100%" alt="Ellipsis">
+                            </div
+                        </div>
+                    <?php } ?>
 
                 <div class="todo-item">
-                    <input type="checkbox">
-                    <h2>Este é um todo list</h2><br>
-                    <small>created: 3/3/2021</small>
-                </div>
-
-            <?php }?>
-
-            <div class="todo-item">
-                    <input type="checkbox">
-                    <h2>Este é um todo list</h2><br>
-                    <small>created: 3/3/2021</small>
-                </div>
+                        <input type="checkbox">
+                        <h2>Este é um todo list</h2><br>
+                        <small>created: 3/3/2021</small>
+                    </div>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
--- src/db_conn.php
+++ src/db_conn.php
@@ -5,7 +5,7 @@ $password = "";
 $dbName = "to_do_list";
 
 try{
-    $conn = new PDO("mysql:host=$hostName;db_name=$dbName", $userName, $password);
+    $conn = new PDO("mysql:host=$hostName;dbname=$dbName", $userName, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     echo("Connecção falhou: ". $e->getMessage());
