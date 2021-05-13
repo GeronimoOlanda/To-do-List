c58bd2b0d85b73a07e1248e81cfc556a909418a2
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-05-13 08:13:11 -0300

    Efetuando conexao com banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require('./src/db_conn.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,14 +19,28 @@
                 <button type="submit">Add &nbsp; <span>&#43;</span></button>
             </form>
         </div>
-
+        
+        <?php 
+            $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+        ?>
         <!-- Exibindo a sessão-->
         <div class="show-todo-section">
+
+            <?php if($todos->rowCount() === 0){ ?>
+
+                <div class="todo-item">
+                    <input type="checkbox">
+                    <h2>Este é um todo list</h2><br>
+                    <small>created: 3/3/2021</small>
+                </div>
+
+            <?php }?>
+
             <div class="todo-item">
-                <input type="checkbox">
-                <h2>Este é um todo list</h2><br>
-                <small>created: 3/3/2021</small>
-            </div>
+                    <input type="checkbox">
+                    <h2>Este é um todo list</h2><br>
+                    <small>created: 3/3/2021</small>
+                </div>
         </div>
     </div>
 </body>
---
+++ src/db_conn.php
@@ -0,0 +1,12 @@
+<?php
+$hostName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "to_do_list";
+
+try{
+    $conn = new PDO("mysql:host=$hostName;db_name=$dbName", $userName, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo("Connecção falhou: ". $e->getMessage());
+}
